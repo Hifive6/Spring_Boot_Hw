2c92bd2835d914149e11bf356d8684952a6428ea
David Juarez <djuar05@gmail.com>
2020-06-18 13:40:35 -0400

    create html css file in view folder and controller

---
+++ src/main/java/com/tts/homework/springbootsite/Controller/VGIController.java
@@ -0,0 +1,9 @@
+package com.tts.homework.springbootsite.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//Video Game info Controller
+public class VGIController {
+     
+}
